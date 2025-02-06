3740c8190dc8d219038fa517b2c0aceec5ebe58b
hafsatcodes <65682252+hafsatcodes@users.noreply.github.com>
2025-02-06 15:55:55 +0100

    chore: add new changes

--- src/components/HeroImage/index.tsx
+++ src/components/HeroImage/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import herophone from "../../../public/assets/herophone.svg";
 import food from "../../../public/assets/food.svg";
 import line from "../../../public/assets/line.svg";
@@ -8,17 +7,15 @@ import styles from "./heroImage.module.css";
 export const Heroimage = () => {
   return (
     <div className={styles.heroContainer}>
-      <Image
-        src={herophone}
+      <img
+        src={herophone.src}
         alt="heroImage"
         className={styles.heroPhone}
         width={471}
         height={493}
-        unoptimized={true}
-        quality={100}
       />
-      <Image src={food} alt="food" className={styles.food} />
-      <Image src={line} alt="line" className={styles.line} />
+      <img src={food.src} alt="food" className={styles.food} />
+      <img src={line.src} alt="line" className={styles.line} />
     </div>
   );
 };
