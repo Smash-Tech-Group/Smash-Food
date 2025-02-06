ebdfdb968a342d4b46c2c1f14432e65a95892efa
hafsatcodes <65682252+hafsatcodes@users.noreply.github.com>
2025-02-06 15:12:08 +0100

    chore: new changes

--- src/components/HeroImage/index.tsx
+++ src/components/HeroImage/index.tsx
@@ -14,7 +14,7 @@ export const Heroimage = () => {
         className={styles.heroPhone}
         width={471}
         height={493}
-        quality={100}
+        unoptimized
       />
       <Image src={food} alt="food" className={styles.food} />
       <Image src={line} alt="line" className={styles.line} />
