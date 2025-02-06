61e85846d5296d4eedc38e5237699468486ab184
hafsatcodes <65682252+hafsatcodes@users.noreply.github.com>
2025-02-06 13:21:22 +0100

    feat: change heroPhone image src

--- src/components/HeroImage/index.tsx
+++ src/components/HeroImage/index.tsx
@@ -8,7 +8,13 @@ import styles from "./heroImage.module.css";
 export const Heroimage = () => {
   return (
     <div className={styles.heroContainer}>
-      <Image src={heroPhone} alt="heroImage" className={styles.heroPhone} />
+      <Image
+        src="/assets/heroPhone.svg"
+        alt="heroImage"
+        className={styles.heroPhone}
+        width={471}
+        height={493}
+      />
       <Image src={food} alt="food" className={styles.food} />
       <Image src={line} alt="line" className={styles.line} />
     </div>
