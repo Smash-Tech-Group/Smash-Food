815196bcc6859db3f82228cf1328b523fc4b1edd
hafsatcodes <65682252+hafsatcodes@users.noreply.github.com>
2025-02-07 08:38:17 +0100

    chore: new changes

--- src/components/HeroImage/index.tsx
+++ src/components/HeroImage/index.tsx
@@ -4,19 +4,47 @@ import herophone from "../../../public/assets/herophone.svg";
 import food from "../../../public/assets/food.svg";
 import line from "../../../public/assets/line.svg";
 import styles from "./heroImage.module.css";
+import { useMedia } from "react-use";
 
 export const Heroimage = () => {
+  const isMobile = useMedia("(max-width: 480px)");
+  const isTablet = useMedia("(min-width: 481px) and (max-width: 768px)");
+  const isDesktop = useMedia("(min-width: 769px)");
+
   return (
     <div className={styles.heroContainer}>
-      <Image
-        src={herophone}
-        alt="heroImage"
-        className={styles.heroPhone}
-        width={471}
-        height={493}
-        unoptimized={true}
-        quality={100}
-      />
+      {isMobile ? (
+        <Image
+          src={herophone}
+          alt="heroImage"
+          className={styles.heroPhone}
+          width={259.05}
+          height={271.15}
+          unoptimized={true}
+          quality={100}
+        />
+      ) : isTablet ? (
+        <Image
+          src={herophone}
+          alt="heroImage"
+          className={styles.heroPhone}
+          width={211.95}
+          height={221.85}
+          unoptimized={true}
+          quality={100}
+        />
+      ) : (
+        <Image
+          src={herophone}
+          alt="heroImage"
+          className={styles.heroPhone}
+          width={471}
+          height={493}
+          unoptimized={true}
+          quality={100}
+        />
+      )}
+
       <Image src={food} alt="food" className={styles.food} />
       <Image src={line} alt="line" className={styles.line} />
     </div>
